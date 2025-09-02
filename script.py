76a9515884a1c017ad85a8e888bb668bd2038166
Jeremias Pretto <jeremias.pretto@glovoapp.com>
2025-09-02 15:39:23 +0200

    update script1

--- script.py
+++ script.py
@@ -1,31 +1,22 @@
 import random
 import os
 import requests
+import time
+from datetime import datetime, timezone
 from Crypto.Cipher import AES
 
 FLAG = os.environ.get("FLAG", "glovo{f4k3_fl4g}").encode()
 
 def blend_key(key_bytes):
     key_int = int(key_bytes.hex(), 16)
-    key_int *= random.randint(1, 9999999) 
+    current_ts = int(time.time())
+    print(f"{datetime.fromtimestamp(current_ts, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')} - Starting shuffling process")
+    key_int *= random.randint(1, 9999999)
     key_int *= random.randint(1, 1234567)
-    key_int *= random.randint(1, 99999) 
+    key_int *= random.randint(1, 99999)
     key_int += (998877665544332211 * 13 // 9833 + 123456789)
+    key_int *= current_ts
     return key_int
-
-def create_github_issue(repo,  body, token):
-    url = f"https://api.github.com/repos/{repo}/issues/1/comments"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
-    data = {
-        "body": body
-    }
-    response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()
-    return response.json()
 
 def main():
     key = os.getenv("SECRET_KEY").encode()
@@ -35,15 +26,7 @@ def main():
     shuffled_key = blend_key(key)
 
     body = f"Your shuffled key:\n{shuffled_key}\n\nHere's your encrypted flag:\n{encrypted_flag.hex()}"
-
-    repo = "puckk/testctf"
-    token = os.environ.get("GITHUB_TOKEN") 
-
-    if repo and token:
-        create_github_issue(repo, body, token)
-    else:
-        print("GitHub repository or token not set in environment variables.")
-
+    print(body)
 
 if __name__ == "__main__":
     main()
